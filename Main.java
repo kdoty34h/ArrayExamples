5bc4f95c72868e3448ead63267e564e0d70f900d
kdoty34h <k3v1n.y34h@gmail.com>
2018-10-12 12:20:00 -0700

    ya

--- Main.java
+++ Main.java
@@ -134,11 +134,40 @@ class Main
         
         /*
          * Task 6A. Print the 2nd to (n-1)th elements of ar1
-         * Task 6B: Print out just the odd numbers in ar1
-         * Task 6C: Print out the elements of ar1 when
+         */
+      System.out.println(" ** Task 6A **");
+        
+        
+ // print 2nd to (n-1)th element
+  for (int i=1 ; i<ar1.length-1 ; i++)
+      System.out.println(ar1[i]);
+      
+        /* Task 6B: Print out just the odd numbers in ar1
+         */
+        
+        System.out.println(" ** Task 6B **");
+        // print odd numbers in ar1
+        // AKA if ar1[i] is odd, print it out.
+        for (int i=0 ; i<ar1.length ; i++)
+            if (ar1[i]%2==1)
+                System.out.println(ar1[i]);
+        
+         /* Task 6C: Print out the elements of ar1 when
          *          the indices are multiples of 3
          *         
          */
+        System.out.println(" ** Task 6C **");
+        // print elements if it is a multiple of 3
+        // AKA if ar1[i] is divisble by 3, print it out.
+        // AKA if ar1[2] has a zero as the remainer when divided by 3
+        for (int i=0 ; i<ar1.length ; i++)
+            if (ar1[i]%3==0)
+                System.out.println(ar1[i]);
+                
+          // Inidices are divicisble by 3
+        for (int i=0 ; i<ar1.length ; i++)
+            if (i%3==0)
+                System.out.println(ar1[i]);
         
         /*
          * Task 7.  For each element in ar1, 
@@ -151,6 +180,8 @@ class Main
          *          ar[2]=30
          *          ar[3]=4
          */
+        
+        
         
          /*
           * Task 8
